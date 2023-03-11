ae1a7545ae4e90fff5d83f873e020eeff16783c5
jyotiprajapti <jyotiprajapati3029@gmail.com>
2023-03-11 15:48:54 +0530

    UC2 created

--- MyMapNode.java
+++ MyMapNode.java
@@ -30,6 +30,36 @@ public class MyMapNode {
         for (MyMapNode node : linkedList) {
             System.out.println(node.key + ": " + node.value);
         }
-    }
-}
 
+                LinkedList<MyMapNode>[] hashTable = new LinkedList[1000];
+                String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+                String[] words2 = paragraph.split("[\\s,;:.!?]+");
+                for (String word : words2) {
+                    int hashCode = word.hashCode() % hashTable.length;
+                    if (hashCode < 0) {
+                        hashCode += hashTable.length;
+                    }
+                    if (hashTable[hashCode] == null) {
+                        hashTable[hashCode] = new LinkedList<>();
+                    }
+                    boolean found = false;
+                    for (MyMapNode node : hashTable[hashCode]) {
+                        if (node.key.equals(word)) {
+                            node.value++;
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        hashTable[hashCode].add(new MyMapNode(word, 1));
+                    }
+                }
+                for (LinkedList<MyMapNode> list : hashTable) {
+                    if (list != null) {
+                        for (MyMapNode node : list) {
+                            System.out.println(node.key + ": " + node.value);
+                        }
+                    }
+                }
+            }
+        }
